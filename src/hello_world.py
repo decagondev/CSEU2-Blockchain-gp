9f7f71b642f8dae99b02cf7d0713f5d4fc63632a
Tom <tomtarpeydev@gmail.com>
2019-10-30 09:38:03 +0000

    hello flask

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,1 +1,14 @@
 # Hello World in flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello CSEU2!"
+
+@app.route("/sum")
+def sum():
+    return f"2 * 4 = {2 * 4}"
+
+if __name__ == '__main__':
+    app.run()
