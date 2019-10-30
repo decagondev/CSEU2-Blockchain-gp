cd154eccc66d9d5eaeb326cdb56c0226b235eac6
Tom <tomtarpeydev@gmail.com>
2019-10-30 11:04:23 +0000

    fixed mining endpoint

--- src/basic_block.py
+++ src/basic_block.py
@@ -2,56 +2,53 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
+
 from flask import Flask, jsonify, request
+
+import sys
 
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
-        # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
         """
         Create a new Block in the Blockchain
-
-        A block should have:
-        * Index
-        * Timestamp
-        * List of current transactions
-        * The proof used to mine this block
-        * The hash of the previous block
-
         :param proof: <int> The proof given by the Proof of Work algorithm
         :param previous_hash: (Optional) <str> Hash of previous Block
         :return: <dict> New Block
         """
 
         block = {
-            'index': len(self.chain) + 1, # length of the chain plus 1
-            'timestamp': time(), # use the time function
-            'transactions': self.current_transactions, # ref to the current transactions of the blockchain
-            'proof': proof, # the proof provided to the function
-            'previous_hash': previous_hash or self.hash(self.chain[-1]) # the hash supplied to the function or the one derived from previous block
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
         # Reset the current list of transactions
         self.current_transactions = []
-        # Append the block to the chain
-        self.chain.append(block)
 
-        # Return the new block
+        self.chain.append(block)
         return block
 
-    def hash(self, block):
+    @staticmethod
+    def hash(block):
         """
         Creates a SHA-256 hash of a Block
-
         :param block": <dict> Block
         "return": <str>
         """
+
+        # Two line version:  
+        # block_string = json.dumps(block, sort_keys=True).encode()
+        # return hashlib.sha256(block_string).hexdigest()
 
         # Use json.dumps to convert json into a string
         # Use hashlib.sha256 to create a hash
@@ -60,29 +57,26 @@ class Blockchain(object):
         # It convertes the string to bytes.
         # We must make sure that the Dictionary is Ordered,
         # or we'll have inconsistent hashes
+
         string_object = json.dumps(block, sort_keys=True)
-
-        # Create the block_string
         block_string = string_object.encode()
-        
 
-        # Hash this string using sha256
         raw_hash = hashlib.sha256(block_string)
+        hex_hash = raw_hash.hexdigest()
 
         # By itself, the sha256 function returns the hash in a raw string
         # that will likely include escaped characters.
         # This can be hard to read, but .hexdigest() converts the
         # hash to a string of hexadecimal characters, which is
         # easier to work with and understand
-        hex_hash = raw_hash.hexdigest()
-        # Return the hashed block string in hexadecimal format
+
         return hex_hash
 
     @property
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, block):
+    def proof_of_work(self):
         """
         Simple Proof of Work Algorithm
         Stringify the block and look for a proof.
@@ -90,17 +84,12 @@ class Blockchain(object):
         in an effort to find a number that is a valid proof
         :return: A valid proof for the provided block
         """
-
-        # create a block string from the encoded dump
+        # One line version of code to stringify a block
         block_string = json.dumps(self.last_block, sort_keys=True).encode()
-        # set initial proof value
         proof = 0
-        # loop over the proofs and check if valid
         while self.valid_proof(block_string, proof) is False:
-            # increment proof
             proof += 1
 
-        # return proof
         return proof
 
     @staticmethod
@@ -115,11 +104,8 @@ class Blockchain(object):
         correct number of leading zeroes.
         :return: True if the resulting hash is a valid proof, False otherwise
         """
-        # encode blockstring and proof to genereate a guess
-        guess = f"{block_string}{proof}".encode()
-        # hash the guess
+        guess = f'{block_string}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        # return True or False
         return guess_hash[:3] == "000"
 
 
@@ -135,14 +121,11 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    # Run the proof of work algorithm to get the next proof
-    proof = blockchain.proof_of_work(blockchain.chain[-1])
-    # Forge the new Block by adding it to the chain with the proof
-    # create a new transaction and reward the proof
-    # TODO: blockchain.new_transaction(sender="0", recipient=node_identifier, amount=1)
-    # previous hash
+    # We run the proof of work algorithm to get the next proof...
+    proof = blockchain.proof_of_work()
+
+    # Forge the new Block by adding it to the chain
     previous_hash = blockchain.hash(blockchain.last_block)
-    # forge new block
     block = blockchain.new_block(proof, previous_hash)
 
     response = {
@@ -150,18 +133,16 @@ def mine():
         'index': block['index'],
         'transactions': block['transactions'],
         'proof': block['proof'],
-        'previous_hash': block['previous_hash']
+        'previous_hash': block['previous_hash'],
     }
-
     return jsonify(response), 200
 
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
-        # Return the chain and its current length
-        'length': len(blockchain.chain), # length
-        'chain': blockchain.chain # chain
+        'length': len(blockchain.chain),
+        'chain': blockchain.chain,
     }
     return jsonify(response), 200
 
